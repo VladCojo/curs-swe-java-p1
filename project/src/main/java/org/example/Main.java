e38c6cc8caf1a3339e37659ca2a5eb0f24e91729
VladCojo <vladvcojocaru@gmail.com>
2024-09-14 11:35:19 +0300

    test

---
+++ project/src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the absolute path of your file: ");
+        String path = scanner.nextLine();
+
+        System.out.print("Enter a word/sentence: ");
+        String word = scanner.nextLine();
+
+
+
+    }
+}
